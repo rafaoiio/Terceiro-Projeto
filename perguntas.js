0b120deb5585a5ae181eec792377eec225aa9138
Rafa_gd <141789950+rafaoiio@users.noreply.github.com>
2024-10-09 23:34:06 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
     'Programação',
-    'O que é Python?',
+    'O que é Python? A COBRA!',
     'O Python é uma linguagem de programação'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
+    'Qual a capital da França? URUGuai não?',
     'A capital da França é Paris'
 )
 
 criaCartao(
     'Programação',
-    'O que é uma função?',
+    'O que é uma função? Jogadô?',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
+    'Como se diz oi em Inglês? Tell us',
     'Oi em ingles é HI (RAI)'
 )
